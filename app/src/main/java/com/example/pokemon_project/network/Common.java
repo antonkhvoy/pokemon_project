c636abdd0c1ad5c3183139a3aa573f97be358583
antonkhvoy <antonkhvoy@gmail.com>
2023-11-30 23:56:28 +0300

    Pokemon v0.1

--- app/src/main/java/com/example/pokemon_project/network/Common.java
+++
@@ -1,9 +0,0 @@
-package com.example.pokemon_project.network;
-
-public class Common {
-    private static final String BASE_URL = "https://api.pokemontcg.io/v2/";
-
-    public static RetrofitService getRetrofitService() {
-        return RetrofitClient.getClient(BASE_URL).create(RetrofitService.class);
-    }
-}
