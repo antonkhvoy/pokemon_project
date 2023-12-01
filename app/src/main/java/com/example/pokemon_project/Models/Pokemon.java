d7ecf249580cdc7b6a885906284aed70577c555a
antonkhvoy <antonkhvoy@gmail.com>
2023-12-01 15:22:21 +0300

    Pokemon v0.1

--- app/src/main/java/com/example/pokemon_project/Models/Pokemon.java
+++ app/src/main/java/com/example/pokemon_project/Models/Pokemon.java
@@ -1,6 +1,4 @@
 package com.example.pokemon_project.Models;
-
-
 import java.util.List;
 
 public class Pokemon {
@@ -9,14 +7,6 @@ public class Pokemon {
     private int hp;
     private List<Attack> attacks;
 
-    /**
-     * Конструктор для создания объекта Pokemon.
-     *
-     * @param name             Имя покемона.
-     * @param imageResourceId  Идентификатор ресурса изображения покемона.
-     * @param hp               Количество здоровья покемона.
-     * @param attacks          Список атак, которые покемон может выполнять.
-     */
     public Pokemon(String name, String imageResourceId, int hp, List<Attack> attacks) {
         this.name = name;
         this.imageResourceId = imageResourceId;
